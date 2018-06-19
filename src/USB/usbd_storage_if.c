137711c2197bc6dc8dfe3e47a68aad157c6c8e13
bekir <bekir.bekirov@l1inc.com>
2018-06-19 23:55:31 +0300

    Init commit

---
+++ src/USB/usbd_storage_if.c
@@ -0,0 +1,459 @@
+#include "usbd_storage_if.h"
+/* USER CODE BEGIN INCLUDE */
+/* USER CODE END INCLUDE */
+
+
+
+#define STORAGE_LUN_NBR                 1
+#define STORAGE_BLK_NBR                 ((uint32_t)384)//128
+#define STORAGE_BLK_SIZ                 ((uint32_t)512)
+#define STORAGE_CAPACITY                                (STORAGE_BLK_NBR * STORAGE_BLK_SIZ)
+#define RAM_FOR_STORE_DATA	((uint32_t)0x20000)	// 128 kbyte
+#define PAGE_SIZE	((uint32_t)0x20000)
+#define PAGE_CNT	2
+#define BLK_PER_PAGE (PAGE_SIZE/STORAGE_BLK_SIZ)
+#define NUM_SECTORS	2
+//#define USE_NUM_SECTORS ((uint32_t)(RAM_FOR_STORE_DATA/STORAGE_CAPACITY))
+
+
+
+/* Base address of the Flash sectors */
+#define ADDR_FLASH_SECTOR_0     ((uint32_t)0x08000000) /* Base @ of Sector 0, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_1     ((uint32_t)0x08004000) /* Base @ of Sector 1, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_2     ((uint32_t)0x08008000) /* Base @ of Sector 2, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_3     ((uint32_t)0x0800C000) /* Base @ of Sector 3, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_4     ((uint32_t)0x08010000) /* Base @ of Sector 4, 64 Kbytes */
+#define ADDR_FLASH_SECTOR_5     ((uint32_t)0x08020000) /* Base @ of Sector 5, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_6     ((uint32_t)0x08040000) /* Base @ of Sector 6, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_7     ((uint32_t)0x08060000) /* Base @ of Sector 7, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_8     ((uint32_t)0x08080000) /* Base @ of Sector 8, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_9     ((uint32_t)0x080A0000) /* Base @ of Sector 9, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_10    ((uint32_t)0x080C0000) /* Base @ of Sector 10, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_11    ((uint32_t)0x080E0000) /* Base @ of Sector 11, 128 Kbytes */
+
+
+
+#define FLASH_USER_START_ADDR   ADDR_FLASH_SECTOR_10  /* Start @ of user Flash area */
+#define FLASH_USER_END_ADDR     ADDR_FLASH_SECTOR_10  +  GetSectorSize(ADDR_FLASH_SECTOR_11) -1 /* End @ of user Flash area : sector start address + sector size -1 */
+
+
+
+uint8_t usbMassStorage[RAM_FOR_STORE_DATA];
+//uint8_t usbMassStorage[0x8000];
+//uint8_t    flashMemoryFlag[8];
+//uint8_t    flashMemoryFlag[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+//uint8_t        fl = 0;
+
+
+#define WAIT_TIMEOUT                                        5000
+
+
+
+/* USB Mass storage Standard Inquiry Data */
+const int8_t  STORAGE_Inquirydata_FS[] = {/* 36 */
+
+  /* LUN 0 */
+  0x00,
+  0x80,
+  0x02,
+  0x02,
+  (STANDARD_INQUIRY_DATA_LEN - 5),
+  0x00,
+  0x00,
+  0x00,
+  'S', 'T', 'M', ' ', ' ', ' ', ' ', ' ', /* Manufacturer : 8 bytes */
+  'P', 'r', 'o', 'd', 'u', 'c', 't', ' ', /* Product      : 16 Bytes */
+  ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+  '0', '.', '0' ,'1',                     /* Version      : 4 Bytes */
+};
+
+
+extern USBD_HandleTypeDef hUsbDeviceFS;
+
+
+
+FLASH_EraseInitTypeDef EraseInitStruct;
+uint32_t SectorError=0;
+
+static int8_t STORAGE_Init_FS (uint8_t lun);
+static int8_t STORAGE_GetCapacity_FS (uint8_t lun,
+                           uint32_t *block_num,
+                           uint16_t *block_size);
+static int8_t  STORAGE_IsReady_FS (uint8_t lun);
+static int8_t  STORAGE_IsWriteProtected_FS (uint8_t lun);
+static int8_t STORAGE_Read_FS (uint8_t lun,
+                        uint8_t *buf,
+                        uint32_t blk_addr,
+                        uint16_t blk_len);
+static int8_t STORAGE_Write_FS (uint8_t lun,
+                        uint8_t *buf,
+                        uint32_t blk_addr,
+                        uint16_t blk_len);
+static int8_t STORAGE_GetMaxLun_FS (void);
+
+
+
+/* USER CODE BEGIN PRIVATE_FUNCTIONS_DECLARATION */
+static uint32_t GetSector(uint32_t Address);
+static uint32_t GetSectorSize(uint32_t Sector);
+/* USER CODE END PRIVATE_FUNCTIONS_DECLARATION */
+
+USBD_StorageTypeDef USBD_Storage_Interface_fops_HS =
+{
+  STORAGE_Init_FS,
+  STORAGE_GetCapacity_FS,
+  STORAGE_IsReady_FS,
+  STORAGE_IsWriteProtected_FS,
+  STORAGE_Read_FS,
+  STORAGE_Write_FS,
+  STORAGE_GetMaxLun_FS,
+  (int8_t *)STORAGE_Inquirydata_FS,
+};
+
+
+
+/* Private functions ---------------------------------------------------------*/
+/*******************************************************************************
+* Function Name  : STORAGE_Init_FS
+* Description    :
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int8_t STORAGE_Init_FS (uint8_t lun)
+{
+  HAL_FLASH_Unlock();
+
+    EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
+    EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+    EraseInitStruct.Sector = FLASH_SECTOR_10;
+    EraseInitStruct.NbSectors = 1;
+
+    // check what blocks in flash is used
+    /*uint8_t i = 0;
+    uint16_t j = 0;
+    uint32_t addr = 0;
+    if(!fl)
+    {
+
+        for (i = 0; i < STORAGE_BLK_NBR; i++)
+        {
+                for(j = 0; j < STORAGE_BLK_SIZ; j++)
+                {
+                    addr = FLASH_USER_START_ADDR + (i*STORAGE_BLK_SIZ) + j;
+                    if (*((volatile uint8_t*)(addr)) != 0xff)
+                    {
+                        flashMemoryFlag[i/8] |= (1 << (i%8));
+                        break;
+                    }
+
+                }
+        }
+        fl = 1;
+    }*/
+
+    //HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError);
+
+  return (USBD_OK);
+}
+
+
+
+/*******************************************************************************
+* Function Name  : STORAGE_GetCapacity_FS
+* Description    :
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int8_t STORAGE_GetCapacity_FS (uint8_t lun, uint32_t *block_num, uint16_t *block_size)
+{
+  /* USER CODE BEGIN 3 */
+  *block_num  = STORAGE_BLK_NBR;
+  *block_size = STORAGE_BLK_SIZ;
+  return (USBD_OK);
+  /* USER CODE END 3 */
+}
+
+
+
+/*******************************************************************************
+* Function Name  : STORAGE_IsReady_FS
+* Description    :
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int8_t  STORAGE_IsReady_FS (uint8_t lun)
+{
+  /* USER CODE BEGIN 4 */
+  return (USBD_OK);
+  /* USER CODE END 4 */
+}
+
+
+
+/*******************************************************************************
+* Function Name  : STORAGE_IsWriteProtected_FS
+* Description    :
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int8_t  STORAGE_IsWriteProtected_FS (uint8_t lun)
+{
+  /* USER CODE BEGIN 5 */
+  return (USBD_OK);
+  /* USER CODE END 5 */
+}
+
+
+
+/*******************************************************************************
+* Function Name  : STORAGE_Read_FS
+* Description    :
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int8_t STORAGE_Read_FS (uint8_t lun,
+                        uint8_t *buf,
+                        uint32_t blk_addr,
+                        uint16_t blk_len)
+{
+  uint32_t i;
+    switch(lun)
+    {
+        case 0:
+            for(i=0; i < (blk_len*STORAGE_BLK_SIZ); i+=1)
+            {
+                buf[i] = *((uint32_t*)(FLASH_USER_START_ADDR + (blk_addr*STORAGE_BLK_SIZ)+ i));
+            }
+            break;
+        case 1:
+            break;
+        default:
+            return (USBD_FAIL);
+    }
+
+  return (USBD_OK);
+
+}
+
+
+
+/*******************************************************************************
+* Function Name  : STORAGE_Write_FS
+* Description    :
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int8_t STORAGE_Write_FS (uint8_t lun,
+                         uint8_t *buf,
+                         uint32_t blk_addr,
+                         uint16_t blk_len)
+{
+  uint32_t sector_num=0;
+  uint32_t addr;
+  uint32_t i,j;
+
+    switch(lun)
+    {
+        case 0:
+        	HAL_FLASH_Unlock();
+        	if (blk_addr<BLK_PER_PAGE)
+        	{
+        		sector_num=0;
+        		i=0; j=0;
+        		addr=blk_addr*STORAGE_BLK_SIZ;
+
+        	}
+        	else
+        	{
+        		sector_num=1;
+        		i=1;
+        		addr=(blk_addr*STORAGE_BLK_SIZ)-PAGE_SIZE;
+        	}
+
+            for (uint32_t k=i; k<=sector_num; k++)
+            {
+        	// copy
+				for (uint32_t n = 0; n < RAM_FOR_STORE_DATA; n++)
+				{
+					usbMassStorage[n] = *((volatile uint8_t*)((FLASH_USER_START_ADDR+(sector_num*RAM_FOR_STORE_DATA)) + n));
+				}
+				// erase
+			    EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
+			    EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+			    EraseInitStruct.Sector = FLASH_SECTOR_10+sector_num;
+			    EraseInitStruct.NbSectors = 1;
+				HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError);
+
+				FLASH_WaitForLastOperation(WAIT_TIMEOUT);
+
+				//modify
+				while ((addr<PAGE_SIZE)&&(j<(blk_len*STORAGE_BLK_SIZ)))
+				{
+					usbMassStorage[addr] = buf[j];
+					j++;
+					addr++;
+				}
+
+				//write
+				for (uint32_t n = 0; n < RAM_FOR_STORE_DATA; n++)
+				{
+					HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE, ((FLASH_USER_START_ADDR+(sector_num*RAM_FOR_STORE_DATA))+ n), usbMassStorage[n]);
+				}
+
+				FLASH_WaitForLastOperation(WAIT_TIMEOUT);
+				//break;
+
+				if (addr >= (blk_addr*STORAGE_BLK_SIZ + (blk_len*STORAGE_BLK_SIZ)))
+				{
+					addr=0;
+					sector_num=1;
+				}
+				else
+				{
+					break;
+				}
+            }
+            HAL_FLASH_Lock();
+            break;
+        case 1:
+            break;
+        default:
+            return (USBD_FAIL);
+    }
+
+  return (USBD_OK);
+  /* USER CODE END 7 */
+}
+
+
+
+/*******************************************************************************
+* Function Name  : STORAGE_GetMaxLun_FS
+* Description    :
+* Input          : None.
+* Output         : None.
+* Return         : None.
+*******************************************************************************/
+int8_t STORAGE_GetMaxLun_FS (void)
+{
+  /* USER CODE BEGIN 8 */
+  return (STORAGE_LUN_NBR - 1);
+  /* USER CODE END 8 */
+}
+
+
+
+/* USER CODE BEGIN PRIVATE_FUNCTIONS_IMPLEMENTATION */
+/**
+  * @brief  Gets the sector of a given address
+  * @param  None
+  * @retval The sector of a given address
+  */
+static uint32_t GetSector(uint32_t Address)
+{
+  uint32_t sector = 0;
+
+  if((Address < ADDR_FLASH_SECTOR_1) && (Address >= ADDR_FLASH_SECTOR_0))
+  {
+    sector = FLASH_SECTOR_0;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_2) && (Address >= ADDR_FLASH_SECTOR_1))
+  {
+    sector = FLASH_SECTOR_1;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_3) && (Address >= ADDR_FLASH_SECTOR_2))
+  {
+    sector = FLASH_SECTOR_2;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_4) && (Address >= ADDR_FLASH_SECTOR_3))
+  {
+    sector = FLASH_SECTOR_3;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_5) && (Address >= ADDR_FLASH_SECTOR_4))
+  {
+    sector = FLASH_SECTOR_4;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_6) && (Address >= ADDR_FLASH_SECTOR_5))
+  {
+    sector = FLASH_SECTOR_5;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_7) && (Address >= ADDR_FLASH_SECTOR_6))
+  {
+    sector = FLASH_SECTOR_6;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_8) && (Address >= ADDR_FLASH_SECTOR_7))
+  {
+    sector = FLASH_SECTOR_7;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_9) && (Address >= ADDR_FLASH_SECTOR_8))
+  {
+    sector = FLASH_SECTOR_8;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_10) && (Address >= ADDR_FLASH_SECTOR_9))
+  {
+    sector = FLASH_SECTOR_9;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_11) && (Address >= ADDR_FLASH_SECTOR_10))
+  {
+    sector = FLASH_SECTOR_10;
+  }
+  else /* (Address < FLASH_END_ADDR) && (Address >= ADDR_FLASH_SECTOR_11) */
+  {
+    sector = FLASH_SECTOR_11;
+  }
+
+
+
+  return sector;
+}
+
+
+
+/**
+  * @brief  Gets sector Size
+  * @param  None
+  * @retval The size of a given sector
+  */
+static uint32_t GetSectorSize(uint32_t Sector)
+{
+  uint32_t sectorsize = 0x00;
+
+
+
+  if((Sector == FLASH_SECTOR_0) || (Sector == FLASH_SECTOR_1) || (Sector == FLASH_SECTOR_2) || (Sector == FLASH_SECTOR_3))
+  {
+    sectorsize = 16 * 1024;
+  }
+  else if(Sector == FLASH_SECTOR_4)
+  {
+    sectorsize = 64 * 1024;
+  }
+  else
+  {
+    sectorsize = 128 * 1024;
+  }
+  return sectorsize;
+}
+
+
+
+/* USER CODE END PRIVATE_FUNCTIONS_IMPLEMENTATION */
+
+
+
+/**
+  * @}
+  */
+
+
+
+/**
+  * @}
+  */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ src/main.c
@@ -0,0 +1,219 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2014 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "diag/Trace.h"
+#include "stm32f4xx_hal.h"
+#include "usb_device.h"
+
+// ----------------------------------------------------------------------------
+//
+// Standalone STM32F4 empty sample (trace via NONE).
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the NONE output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+int cnt;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+
+int main(int argc, char* argv[])
+{
+	  /* USER CODE BEGIN 1 */
+
+	  /* USER CODE END 1 */
+
+	  /* MCU Configuration----------------------------------------------------------*/
+
+	  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	  HAL_Init();
+
+	  /* USER CODE BEGIN Init */
+
+	  /* USER CODE END Init */
+
+	  /* Configure the system clock */
+	  SystemClock_Config();
+
+	  /* USER CODE BEGIN SysInit */
+
+	  /* USER CODE END SysInit */
+
+	  /* Initialize all configured peripherals */
+	  MX_GPIO_Init();
+	  MX_USB_DEVICE_Init();
+	  /* USER CODE BEGIN 2 */
+
+	  /* USER CODE END 2 */
+
+	  /* Infinite loop */
+	  /* USER CODE BEGIN WHILE */
+	  while (1)
+	  {
+
+	  /* USER CODE END WHILE */
+
+	  /* USER CODE BEGIN 3 */
+
+	  }
+
+}
+
+	/**
+	  * @brief System Clock Configuration
+	  * @retval None
+	  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Configure the main internal regulator output voltage
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 288;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 6;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+	/** Configure pins as
+	        * Analog
+	        * Input
+	        * Output
+	        * EVENT_OUT
+	        * EXTI
+	*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+}
+
+
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
